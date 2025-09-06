ad1e30eada1cb9ecbb268e976f686b488e10498a
Ehiomhen Treasure <ehiomhentreasureruth@gmail.com>
2025-09-06 23:18:06 +0100

    Add Flask dog generator app

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import Flask
+from flask import render_template
+import requests # type: ignore
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello from Flask backend!"
+
+@app.route('/')
+def index():
+    dog_pic = get_dog()
+    return render_template("index.html", dog_pic=dog_pic)
+
+
+def get_dog():
+    url = "https://dog.ceo/api/breeds/image/random"
+    try:
+        response = requests.get(url, timeout=5).json()
+        return response["message"] 
+    except Exception as e:
+        print("API error", e)
+        return "https://via.placeholder.com/300?text=No+Meme", "unknown"
+   
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-@app.route("/api/message")
-def message():
-    return {"message": "Hello from your API!"}
