1ea1f26b14afcf4b9c97ecd6248277a1765ea252
Ehiomhen Treasure <ehiomhentreasureruth@gmail.com>
2025-09-07 21:53:41 +0100

    A few minor improvements

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 import requests # type: ignore
 
@@ -8,7 +8,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     dog_pic = get_dog()
-    return render_template("index.html", dog_pic=dog_pic)
+    cat_pic = get_cat()
+    dog_result = None
+    cat_result = None
+    return render_template("index.html", 
+                           cat_pic=cat_pic, 
+                           dog_pic=dog_pic,
+                           dog_result=dog_result,
+                           cat_result=cat_result)
 
 
 def get_dog():
@@ -18,6 +25,16 @@ def get_dog():
         return response["message"] 
     except Exception as e:
         print("API error", e)
+        return "https://via.placeholder.com/300?text=No+Meme", "unknown"
+    
+def get_cat():
+    url = "https://api.thecatapi.com/v1/images/search"
+    try:
+        response = requests.get(url, timeout=5).json()
+        print("cat API response:", response)
+        return response[0]["url"]
+    except Exception as e:
+        print("API Error", e)
         return "https://via.placeholder.com/300?text=No+Meme", "unknown"
    
 
