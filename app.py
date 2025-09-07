dc950f5fc08d057b7606a63038daf4e9cb0e2ca1
Ehiomhen Treasure <ehiomhentreasureruth@gmail.com>
2025-09-07 22:24:29 +0100

    added forms

--- app.py
+++ app.py
@@ -7,22 +7,40 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    dog_pic = get_dog()
-    cat_pic = get_cat()
-    dog_result = None
-    cat_result = None
+    dog_pic, dog_breed = get_dog()
+    cat_pic, cat_breed = get_cat()
+
     return render_template("index.html", 
                            cat_pic=cat_pic, 
                            dog_pic=dog_pic,
-                           dog_result=dog_result,
-                           cat_result=cat_result)
+                           dog_result = None,
+                           cat_result = None,
+                           dog_breed=dog_breed,
+                           cat_breed=cat_breed)
+
+
+
+@app.route('/guess_dog', methods=['POST'])
+def guess_dog():
+    user_guess = request.form.get('dog_guess', '').lower()
+    dog_pic, dog_breed = get_dog()
+    result = "correct!" if user_guess == dog_breed.lower() else f"Wrong it was {dog_breed}"
+    return render_template("index.html", dog_pic=dog_pic,dog_breed=dog_breed, dog_result=result, cat_pic=None)
+
+@app.route('/guess_cat', methods=['POST'])
+def guess_cat():
+    user_guess = request.form.get('cat_guess', '').lower()
+    cat_pic, cat_breed = get_cat()
+    result = "correct!" if user_guess == cat_breed.lower() else f"Wrong this is {cat_breed}"
+    return render_template("index.html", cat_pic=cat_pic, cat_breed=cat_breed, cat_result=result, dog_pic=None)
 
 
 def get_dog():
     url = "https://dog.ceo/api/breeds/image/random"
     try:
-        response = requests.get(url, timeout=5).json()
-        return response["message"] 
+        response = requests.get(url).json()
+        breed = response["message"].split("/")[4]
+        return response["message"], breed
     except Exception as e:
         print("API error", e)
         return "https://via.placeholder.com/300?text=No+Meme", "unknown"
@@ -30,9 +48,10 @@ def get_dog():
 def get_cat():
     url = "https://api.thecatapi.com/v1/images/search"
     try:
-        response = requests.get(url, timeout=5).json()
+        response = requests.get(url).json()
         print("cat API response:", response)
-        return response[0]["url"]
+        breed = response[0].get("breeds", [{"name": "unknown"}])[0]["name"]
+        return response[0]["url"],breed
     except Exception as e:
         print("API Error", e)
         return "https://via.placeholder.com/300?text=No+Meme", "unknown"
